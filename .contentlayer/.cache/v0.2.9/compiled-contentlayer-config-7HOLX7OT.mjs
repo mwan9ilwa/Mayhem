5c5380a308539c7ae5e61c939db1e2a02573fa6e
Mwangilwa Zimba <mwangilwa23@gmail.com>
2023-08-18 20:34:23 +0200

    more fixes patched

--- .contentlayer/.cache/v0.2.9/compiled-contentlayer-config-7HOLX7OT.mjs
+++ .contentlayer/.cache/v0.2.9/compiled-contentlayer-config-7HOLX7OT.mjs
@@ -1,6 +1,6 @@
+// contentlayer.config.ts
 import { defineDocumentType, makeSource } from "contentlayer/source-files";
-
-export const Service = defineDocumentType(() => ({
+var Service = defineDocumentType(() => ({
   name: "Service",
   filePathPattern: `**/*.mdx`,
   contentType: "mdx",
@@ -8,56 +8,56 @@ export const Service = defineDocumentType(() => ({
     title: {
       type: "string",
       description: "The title of the post",
-      required: true,
+      required: true
     },
     description: {
       type: "string",
       description: "The description of the post",
-      required: true,
+      required: true
     },
     category: {
       type: "string",
       description: "The category of the post",
-      required: true,
+      required: true
     },
     date: {
       type: "date",
       description: "The date of the post",
-      required: true,
+      required: true
     },
     author: {
       type: "string",
       description: "The author of the post",
-      required: true,
+      required: true
     },
     tags: {
       type: "json",
       description: "Related concepts to the post",
-      required: true,
+      required: true
     },
     draft: {
       type: "boolean",
       description: "Determines if the post has been published",
-      required: true,
-    },
+      required: true
+    }
   },
   computedFields: {
     slug: {
       type: "string",
-      resolve: (service) => `${service._raw.flattenedPath}`,
+      resolve: (service) => `${service._raw.flattenedPath}`
     },
     url: {
       type: "string",
-      resolve: (service) => `/services/${service._raw.flattenedPath}`,
-    },
-    // tagUrl: {
-    //   type: "string",
-    //   resolve: (service) => `/services/tags/`
-    // }
-  },
+      resolve: (service) => `/services/${service._raw.flattenedPath}`
+    }
+  }
 }));
-
-export default makeSource({
+var contentlayer_config_default = makeSource({
   contentDirPath: "services",
-  documentTypes: [Service],
+  documentTypes: [Service]
 });
+export {
+  Service,
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-7HOLX7OT.mjs.map
